d91efbe85fc2d605e45d06a78358285d1526daa3
Thirumalai15 <m.thirurahul@gmail.com>
2023-01-12 22:55:57 +0530

    encoding url

--- src/Http/Controllers/SsoLoginController.php
+++ src/Http/Controllers/SsoLoginController.php
@@ -35,20 +35,20 @@ class SsoLoginController extends Controller
     public function sso_login(Request $request)
     {
         $app_name = config('app.name');
-        $redirect_url = base64_encode(config('app.url'));
+        $encoded_url = config('app.url');
         $sso_key = $request->cookie('sso_token');
         $sso_url = config('sso.sso_url');
         $homepage_route_name = config('sso.homepage_route_name');
 
-        for ($i = 0; $i < 10; $i++) {
-            $encoded_url = base64_encode($redirect_url);
+        for ($i = 0; $i < 12; $i++) {
+            $encoded_url = base64_encode($encoded_url);
         }
 
         $sso_verify_url = $sso_url . '/api/v1/sso/verify-token/' . $sso_key;
         $response = Http::get($sso_verify_url);
 
         $result = json_decode($response->getBody());
-        // dd($result);
+
         if ($result) {
             if ($result->status === 'ok') {
                 $result_data = $result->data;
@@ -59,7 +59,7 @@ class SsoLoginController extends Controller
                 return redirect($sso_url . '/sso/login?Y21Wa2FYSmxZM1E=' . $encoded_url . '&app_name=' . $app_name);
             }
         } else {
-            // $signed_url = Url::temporarySignedRoute('index', now()->minutes(2), ['Y21Wa2FYSmxZM1E' => $encoded_url], false);
+
             return redirect($sso_url . '/sso/login?Y21Wa2FYSmxZM1E=' . $encoded_url . '&app_name=' . $app_name);
         }
     }
