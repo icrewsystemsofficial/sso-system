dca6dc3a740ce2a040581e36b7b734144d4ac858
Thirumalai15 <m.thirurahul@gmail.com>
2023-01-12 18:37:20 +0530

    [Added] encoding the urls

--- src/Http/Controllers/SsoLoginController.php
+++ src/Http/Controllers/SsoLoginController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\URL;
 
 class SsoLoginController extends Controller
 {
@@ -33,11 +34,15 @@ class SsoLoginController extends Controller
 
     public function sso_login(Request $request)
     {
-        // dd('sso_login');
+        $app_name = config('app.name');
         $redirect_url = base64_encode(config('app.url'));
         $sso_key = $request->cookie('sso_token');
         $sso_url = config('sso.sso_url');
         $homepage_route_name = config('sso.homepage_route_name');
+
+        for ($i = 0; $i < 10; $i++) {
+            $encoded_url = base64_encode($redirect_url);
+        }
 
         $sso_verify_url = $sso_url . '/api/v1/sso/verify-token/' . $sso_key;
         $response = Http::get($sso_verify_url);
@@ -51,10 +56,11 @@ class SsoLoginController extends Controller
                 Auth::login($user);
                 return redirect()->route($homepage_route_name);
             } else {
-                return redirect($sso_url . '/sso/login?redirect_url=' . $redirect_url);
+                return redirect($sso_url . '/sso/login?Y21Wa2FYSmxZM1E=' . $encoded_url . '&app_name=' . $app_name);
             }
         } else {
-            return redirect($sso_url . '/sso/login?redirect_url=' . $redirect_url);
+            // $signed_url = Url::temporarySignedRoute('index', now()->minutes(2), ['Y21Wa2FYSmxZM1E' => $encoded_url], false);
+            return redirect($sso_url . '/sso/login?Y21Wa2FYSmxZM1E=' . $encoded_url . '&app_name=' . $app_name);
         }
     }
 }
